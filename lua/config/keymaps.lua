3e39910318e32baa118ba09a36274a4314e9981f
Fabiano Siqueira <fabiano.siqueirab@gmail.com>
2024-01-21 23:22:30 -0300

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>pv", "<cmd>Ex<cr>", { desc = "open netrw" })
+keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+
